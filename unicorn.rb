58015e3cca16e36bc2411de943d71cb6d0d12c86
Jonas Forsberg <himynameisjonas@gmail.com>
2012-08-04 11:22:45 +0300

    Avoid regeneration of jekyll site for each fork

--- unicorn.rb
+++ unicorn.rb
@@ -1,2 +1,3 @@
 worker_processes 6 # amount of unicorn workers to spin up
 timeout 30         # restarts workers that hang for 30 seconds
+preload_app true   # avoid regeneration of jekyll site for each fork
